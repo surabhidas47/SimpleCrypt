f1db0f6936064c5ce4d3177a34e94e29d9899ea1
surabhi <surabhi.das@outlook.com>
2023-07-30 09:41:53 -0400

    working on crypt

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -4,14 +4,36 @@ import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
 
+    Character cs;
+    Character cf;
+
     ROT13(Character cs, Character cf) {
+
+
+        this.cs=cs;
+
+        this.cf=cf;
+
     }
 
     ROT13() {
+
+
+
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
+
+
+        char [] letters = new char[text.length()];
+
+        for (int i=0;i<text.length();i++){
+            letters[i]=text.charAt(i);
+        }
+
+
+
 
         return "";
     }
